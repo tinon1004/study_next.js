7c820ceb0f8b6fcaa94d6b9c3f9b8350314b1c7a
tinon1004 <tinon1004@ajou.ac.kr>
2023-08-26 22:11:20 +0900

    first day next.js studying

---
+++ src/app/create/page.js
@@ -0,0 +1,9 @@
+
+export default function Create() {
+  return (
+    <div>
+      <h2>Create</h2>
+      HELLO, Create!
+    </div>
+  )
+}
---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    
+      
+    <div>
+      <h2>WEB</h2>
+      HELLO, WEB!
+      <p>
+        <img src="/ham.jpg" alt="ham" width="100" />
+      </p>
+    </div>
+  )
+}
