e9002fbd812dfc842b233e5e02f4e26efd96c9a7
tinon1004 <tinon1004@ajou.ac.kr>
2023-08-27 20:38:46 +0900

    Second day : create server,data and function

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,9 +1,34 @@
+'use client'
+import {useRouter} from 'next/navigation';
 
 export default function Create() {
+
+  const router = useRouter();
+
   return (
-    <div>
+    <form onSubmit ={async e =>{
+      e.preventDefault();
+      const title = e.target.title.value;
+      const body = e.target.body.value;
+      const resp = await fetch('http://localhost:9999/topics',{
+
+        method: 'POST',
+        body: JSON.stringify({title, body}),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+        const topic = await resp.json();
+        const lastId = topic.id;
+        const url = `/read/${lastId}`;
+        router.push(url);
+        router.refresh();
+
+    }}>
       <h2>Create</h2>
-      HELLO, Create!
-    </div>
+      <p><input type="text" name="title" placeholder="title"/></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="create" /></p>
+    </form>
   )
 }
